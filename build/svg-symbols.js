3722d6756618496b2bba215c3bb36829be0add87
LDQ-first <178848890@qq.com>
2017-05-04 07:55:44 +0800

    modify svg-symbols.js

--- build/svg-symbols.js
+++ build/svg-symbols.js
@@ -1,4 +1,20 @@
  /*这是一个 Node.js 脚本，并没有被 webpack 打包，
  所以我们不能使用 import path from 'path'*/
 
- 
+const path = require('path'); // 路径工具
+const fs = require('fs');     // 文件系统工具
+
+let svgFolder = path.join(__dirname, '../static/svg_icons');
+let jsPath = path.join(__dirname, '../src/assets/icons.js');
+
+// [ 'add.svg', 'book.svg', 'cup.svg', 'heart.svg', 'id.svg', 'phone.svg', 'work.svg' ]
+let svgFiles = fs.readdirSync(svgFolder); 
+
+let symbols = svgFiles.map(function(filename) {
+    let absolutePath = path.join(svgFolder, filename);
+    let fileContent = fs.readFileSync(absolutePath).toString('utf8');
+    let name = path.basename(filename, '.svg');
+    return fileContent
+        .repalce()
+})
+
