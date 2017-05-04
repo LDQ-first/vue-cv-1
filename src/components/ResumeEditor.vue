60875c7bdfd193a17644d44b161d8049d24fb84f
LDQ-first <178848890@qq.com>
2017-05-04 14:10:02 +0800

    resume 属性同时支持数组和对象

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -2,23 +2,30 @@
     <div id="resumeEditor">
         <nav>
             <ol>
-                <li v-for="(item, index) in resume.config"
-                 :class="{active: item.field === selected}"
-                 @click="selected = item.field">
-                 <svg class="icon">
-                     <use :xlink:href="`#icon-${item.icon}`"></use>
-                 </svg>
+                <li v-for="(item, index) in resume.config" :class="{active: item.field === selected}" @click="selected = item.field">
+                    <svg class="icon">
+                        <use :xlink:href="`#icon-${item.icon}`"></use>
+                    </svg>
                 </li>
             </ol>
         </nav>
-            <ol class="panels">
-                <li v-for="item in resume.config" v-show="item.field === selected">
-                    <div class="resumeField" v-for="(value, key) in resume[item.field]">
-                        <label>{{key}}</label>
-                        <input type="text" v-model="resume[item.field][key]">
+        <ol class="panels">
+            <li v-for="item in resume.config" v-show="item.field === selected">
+                <div v-if="resume[item.field] instanceof Array">
+                    <div class="subitem" v-for="subitem in resume[item.field]">
+                        <div class="resumeField" v-for="(value, key) in subitem">
+                            <label>{{key}}</label>
+                            <input type="text" :value="value">
+                        </div>
+                        <hr>
                     </div>
-                </li>
-            </ol>
+                </div>
+                <div v-else class="resumeField" v-for="(value, key) in resume[item.field]">
+                    <label>{{key}}</label>
+                    <input type="text" v-model="resume[item.field][key]">
+                </div>
+            </li>
+        </ol>
     </div>
 </template>
 
@@ -46,10 +53,22 @@
                         {company: 'AL', content: '我的第二份工作是'},
                         {company: 'TX', content: '我的第一份工作是'}
                     ],
-                    education: [],
-                    projects: [],
-                    awards: [],
-                    contacts: []
+                    education: [
+                        { school: 'AL', content: '文字' },
+                        { school: 'TX', content: '文字' },
+                    ],
+                    projects: [
+                        { name: 'project A', content: '文字' },
+                        { name: 'project B', content: '文字' },
+                    ],
+                    awards: [
+                        { name: 'awards A', content: '文字' },
+                        { name: 'awards B', content: '文字' },
+                    ],
+                    contacts: [
+                        { contact: 'phone', content: '13812345678' },
+                        { contact: 'qq', content: '12345678' },
+                    ],
                 }
             }
         }
@@ -59,7 +78,7 @@
 <style lang="scss" scoped>
     #resumeEditor {
         background: #FFF;
-        box-shadow: 0 1px 3px rgba(0,0,0,0.25);
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25);
         display: flex;
         flex-direction: row;
         overflow: auto;
@@ -95,7 +114,7 @@
                     label {
                         display: block;
                     }
-                    input[type=text]{
+                    input[type=text] {
                         margin: 16px 0;
                         border: 1px solid #DDD;
                         box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.25);
