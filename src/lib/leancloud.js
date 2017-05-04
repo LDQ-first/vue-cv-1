1a8b27d9fd199086bbabe566d0e2b7b5c11ab5c0
LDQ-first <178848890@qq.com>
2017-05-04 22:08:59 +0800

    SignUpForm.vue

---
+++ src/lib/leancloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage';
+
+var APP_ID = 'v8panTMIAipFkUWxhtoOEiG8-gzGzoHsz';
+var APP_KEY = 'Y7EeqSz2v9BaAKOdSeMTEo2H';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
