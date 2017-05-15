82358e2a3d7fddeaa183c65baa0fe69c26f715f5
LDQ-first <178848890@qq.com>
2017-05-15 17:40:28 +0800

    modify rebuild

--- src/components/rebuild/rebuild.js
+++ src/components/rebuild/rebuild.js
@@ -1,4 +1,4 @@
-
+import changeState from '../../lib/changeState.js'
 
 export default {
     name: 'rebuild',
@@ -13,5 +13,40 @@ export default {
         }
     },
     methods: {
+        rebuild(){
+          /*  console.log(this.$store.state);*/
+            const resume = this.$store.state.resume;
+            const resumeConfig = resume.config;
+            for(let i = 0; i < resumeConfig.length; i++) {
+                const resumeField = resume[resumeConfig[i].field];
+                if(resumeField instanceof Array) {
+                    //console.log(resumeConfig[i].field);
+                    for(let k of resumeField) {
+                        console.log(k);
+                        /*if(k instanceof Array) {
+
+                        }*/
+                      /*  this.clearObj(k, resumeConfig[i].field);*/
+                    }
+                 //   this.clearObj(resumeField, resumeConfig[i].field);
+                }
+                else {                
+                     this.clearObj(resumeField, resumeConfig[i].field);
+                }
+            }
+        },
+        clearObj(obj, field) {
+            console.log(obj,field);
+            /*let copyObj = JSON.parse(JSON.stringify(obj))
+            console.log(copyObj);*/
+            for(let key in obj) {
+                console.log(key);
+                console.log(`resume.${field}.${key}`);
+              //  this.changeResumeField(`resume.${field}.${key}`, '');
+            }
+        },
+        changeResumeField(path, value) {
+            changeState(path, value)
+        },
     }
 }
--- src/components/rebuild/rebuild.vue
+++ src/components/rebuild/rebuild.vue
@@ -1,6 +1,7 @@
 <template>    
     <div id="rebuild" :style="{background: (skinColor === '#FFF' ? '#000': skinColor)}" 
-    @mouseenter="showRebuildText=true" @mouseleave="showRebuildText=false">
+    @mouseenter="showRebuildText=true" @mouseleave="showRebuildText=false" 
+    @click="rebuild">
         <transition name="rebuild" tag="div">
             <span class="rebuild" v-show="showRebuildText">Rebuild</span>
         </transition>
