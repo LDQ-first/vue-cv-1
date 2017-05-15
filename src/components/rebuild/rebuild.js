c0296b0fcf512e4ef17be0cc8be40b874df6e5ae
LDQ-first <178848890@qq.com>
2017-05-15 20:44:25 +0800

    build

--- src/components/rebuild/rebuild.js
+++ src/components/rebuild/rebuild.js
@@ -13,48 +13,38 @@ export default {
         }
     },
     methods: {
-        rebuild(){
-          /*  console.log(this.$store.state);*/
+        rebuild() {
             const resume = this.$store.state.resume;
             const resumeConfig = resume.config;
             this.traversalResume(resume, resumeConfig)
         },
         traversalResume(resume, resumeConfig) {
-            for(let i = 0; i < resumeConfig.length; i++) {
-                const resumeField = resume[resumeConfig[i].field];
+            for(let i of resumeConfig) {
+                const resumeField = resume[i.field];
                 if(resumeField instanceof Array) {
-                    /*console.log(resumeConfig[i].field);*/
                     let j = 0;
                     for(let k of resumeField) {
                         if(k instanceof Object) {
-                            console.log(j);
-                            this.clearObj(k, resumeConfig[i].field, j);
+                            this.clearObj(k, i.field, j);
                             j++;
-                            
                         }
                     }
                 }
                 else {                
-                     this.clearObj(resumeField, resumeConfig[i].field);
+                     this.clearObj(resumeField, i.field);
                 }
             }
+
         },
         clearObj(obj, field, i) {
-            console.log(obj,field);
-            /*let copyObj = JSON.parse(JSON.stringify(obj))
-            console.log(copyObj);*/
             let path ;
             for(let key in obj) {
-             //   console.log(key);
              if(isNaN(i)) {
-                console.log(`resume.${field}.${key}`);
                 path = `resume.${field}.${key}`;
              }
              else {
-                console.log(`resume.${field}.${i}.${key}`);
                 path = `resume.${field}.${i}.${key}`;
              }
-               
                 this.changeResumeField(path, '');
             }
         },
